49e6ff5876ade61240a80143ca752da83bc39237
Shubham <shubhammittal0610@proton.me>
2025-12-19 07:56:04 +0530

    Functional Singleton Logger implemeneted.

--- src/main/java/com/shubham/logger/App.java
+++ src/main/java/com/shubham/logger/App.java
@@ -2,6 +2,12 @@ package com.shubham.logger;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Environment is ready!");
+        Logger logger = Logger.getInstance();
+        logger.log(Loglevel.INFO, "info message.");
+        logger.log(Loglevel.DEBUG, "debug message.");
+        logger.log(Loglevel.ERROR, "error message.");
+
+        Logger logger2 = Logger.getInstance();
+        System.out.println(logger == logger2); // should print true since both are same instance same object == checks reference  points to same memory location
     }
 }
